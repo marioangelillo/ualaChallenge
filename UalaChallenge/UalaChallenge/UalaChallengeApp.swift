beb28f97aa1b109384970439128d505139ea8db3
Mario Angelillo <marioangelillo@gmail.com>
2025-08-02 15:33:10 -0300

    create project

---
+++ UalaChallenge/UalaChallenge/UalaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaChallengeApp.swift
+//  UalaChallenge
+//
+//  Created by Mario Angelillo on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct UalaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
