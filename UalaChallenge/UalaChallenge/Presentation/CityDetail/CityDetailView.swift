510a02b5b7704c9fa9b6c09b0e49b56451f74d1d
Mario Angelillo <marioangelillo@gmail.com>
2025-08-03 20:49:50 -0300

    create screens and connections with viewmodel with mocked data

--- UalaChallenge/UalaChallenge/Presentation/CityDetail/CityDetailView.swift
+++ UalaChallenge/UalaChallenge/Presentation/CityDetail/CityDetailView.swift
@@ -5,3 +5,17 @@
 //  Created by Mario Angelillo on 03/08/2025.
 //
 
+import SwiftUI
+
+struct CityDetailView: View {
+    let city: City
+    
+    var body: some View {
+        VStack {
+            Text(city.name)
+            Text(city.country)
+            Text("latitude: \(city.coordinate.lat)")
+            Text("longitude: \(city.coordinate.lon)")
+        }
+    }
+}
--- UalaChallenge/UalaChallenge/Presentation/Map/MapView.swift
+++ UalaChallenge/UalaChallenge/Presentation/Map/MapView.swift
@@ -5,3 +5,20 @@
 //  Created by Mario Angelillo on 03/08/2025.
 //
 
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @Binding var selectedCity: City?
+    let defaultCoordinates = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+    
+    var body: some View {
+        Map {
+            if let selectedCity {
+                Marker(selectedCity.name, coordinate: selectedCity.cityCoordinates)
+            } else {
+                Marker("Map", coordinate: defaultCoordinates)
+            }
+        }
+    }
+}
